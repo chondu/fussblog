8f3d1e2a9ab33982cb7a49910f8ca536dd03c4de
chondu <112936694+chondu@users.noreply.github.com>
2023-09-05 23:11:08 +0800

    sql test

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
-export default function RootLayout({ children }) {
-  return (
-      <div>
-        hello world
-      </div>
-  )
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({ params }) {
+    const { rows } = await sql`SELECT * from item`;
+
+    return (
+        <div>
+            {rows.map((row) => (
+                <div key={row.item_id}>
+                    {row.item_id} - {row.item_content}
+                </div>
+            ))}
+        </div>
+    );
 }
